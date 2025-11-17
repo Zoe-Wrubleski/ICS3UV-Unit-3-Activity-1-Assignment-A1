df1fb238aeaad13f012d0f57a9d999f69a1d387c
default <zoe.wrubleski@stu.ocsb.ca>
2025-11-17 14:08:32 +0000

    area of a circle calculation

---
+++ main.ts
@@ -0,0 +1,20 @@
+/**
+ * @author Zoe Wrubleski
+ * @version 1.0.0
+ * @date 2025-11-17
+ * @fileoverview This program calculates the area of a circle 
+ */
+
+// set the numbers
+const Pi: number = 3.14;
+let r: number = 5.6 ;
+
+// calculate the area
+let answer: number = Pi * (r * r);
+
+// display the answer
+console.log(
+  "The area of a circle with a radius of " + r + " cm is " + answer + " cm²."
+);
+
+console.log("\nDone.");
